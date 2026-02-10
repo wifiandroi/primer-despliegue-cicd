c0cf3c15797887a872c8f793745bcf9f34fabc75
unknown <dell@PCP-0638.mtpe.gob.pe>
2026-02-09 22:41:23 -0500

    commit código

--- src/main/java/com/github/actions/GithubActionsApplication.java
+++ src/main/java/com/github/actions/GithubActionsApplication.java
@@ -14,6 +14,11 @@ public class GithubActionsApplication {
 		return ":::: Joseph ::: ";
 	}
 
+	@GetMapping("/welcome")
+	public String welcome() {
+		return ":::: Joseph2 ::: ";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsApplication.class, args);
 	}
